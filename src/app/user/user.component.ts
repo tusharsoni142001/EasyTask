eff09046e1f5c128fcc9972e21c56511d018f2ff
Tushar Soni <tusharsoni142001@gmail.com>
2024-12-20 12:59:58 +0530

    First commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  get imgPath()
+  {
+    return "users/" + this.selectedUser.avatar;
+  }
+
+  onUserSelect()
+  {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
+}
