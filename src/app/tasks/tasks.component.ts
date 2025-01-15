7e9ac22a273d894b5fc34120081d63c84849e268
Tushar Soni <tusharsoni142001@gmail.com>
2025-01-15 23:39:19 +0530

    Added new-task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,14 +1,69 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from './new-task/new-task.component';
+import { every } from 'rxjs';
+
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string;
-  // @Input() name: string | undefined;
+  @Input({required:true}) userId!:string;
+  @Input({required:true}) name!: string;
+  
+  @Input() cancel!: boolean;
+  showAddTask: boolean = false;
 
+  dummyTasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ]
+
+  
+  get selectedUserTasks()
+  {
+    return this.dummyTasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id:string)
+  {
+    this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
+  }
+
+  addTaskCheck()
+  {
+    this.showAddTask = !this.showAddTask;
+  }
+
+  onCancelAddTask()
+  {
+    this.showAddTask=false;
+  }
 }
+
+
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,10 @@
 import { Component, computed, EventEmitter, input, Input, Output, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
+import { User } from "./user.model";
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+
 
 @Component({
   selector: 'app-user',
@@ -15,11 +18,8 @@ export class UserComponent {
   // @Input({required: true}) avatar!: string;
   // @Input({required: true}) name!: string;
 
-  @Input({required:true}) user!: {
-    id:string;
-    name:string;
-    avatar:string;
-  }
+  @Input({required:true}) user!: User;
+  @Input({required:true}) selected!:boolean;
   @Output() select = new EventEmitter<string>();
   // select = output<string>
   // avatar = input<string>();
